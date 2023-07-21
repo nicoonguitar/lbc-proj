02051e87a20829392d305e297e1d51e489c9ce68
nicoonguitar <nigarcia88@gmail.com>
2023-07-21 19:36:41 +0200

    Initial Commit

---
+++ Lbc/LbcApp.swift
@@ -0,0 +1,17 @@
+//
+//  LbcApp.swift
+//  Lbc
+//
+//  Created by Nicolás García on 21/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct LbcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
