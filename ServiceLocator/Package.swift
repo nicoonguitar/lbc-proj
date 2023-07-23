b80a5e2c2b599845292e7f0fb250d3df8ceeb9c8
nicoonguitar <nigarcia88@gmail.com>
2023-07-23 20:45:27 +0200

    chg: rm test target

--- ServiceLocator/Package.swift
+++ ServiceLocator/Package.swift
@@ -21,9 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ServiceLocator",
-            dependencies: []),
-        .testTarget(
-            name: "ServiceLocatorTests",
-            dependencies: ["ServiceLocator"]),
+            dependencies: []
+        )
     ]
 )
