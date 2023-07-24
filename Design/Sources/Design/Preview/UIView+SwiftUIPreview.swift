b30ee38454214ffff7ddd970e9203e21a23b5e69
nicoonguitar <nigarcia88@gmail.com>
2023-07-24 03:01:24 +0200

    new: Design package

---
+++ Design/Sources/Design/Preview/UIView+SwiftUIPreview.swift
@@ -0,0 +1,22 @@
+import Foundation
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+struct UIViewPreview<View: UIView>: UIViewRepresentable {
+    let view: View
+    
+    init(_ builder: @escaping () -> View) {
+        view = builder()
+    }
+    
+    func makeUIView(context: Context) -> some UIView {
+        view
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        view.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        view.setContentHuggingPriority(.defaultHigh, for: .vertical)
+    }
+}
+
+#endif
