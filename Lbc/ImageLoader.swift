d25a053d7e8ca978ff7e0cf9d9d787fd9b09b27b
nicoonguitar <nigarcia88@gmail.com>
2023-07-28 01:14:47 +0200

    new: ImageLoader documentation

--- Lbc/ImageLoader.swift
+++ Lbc/ImageLoader.swift
@@ -5,9 +5,16 @@ enum ImageLoaderError: Error, Equatable {
     case badImage
 }
 
+/// The ImageLoader is a Swift implementation of an image loader with asynchronous capabilities using Swift's async and await pattern.
+/// It allows you to fetch images from remote URLs and cache them for efficient reuse.
+/// The loader is implemented as an actor, ensuring safe concurrent access to its internal state.
 actor ImageLoader {
+    
+    /// An enumeration representing the state of an image loading operation.
     enum State {
+        ///  Indicates that an image loading task is in progress and provides a Task representing the ongoing asynchronous image fetching operation.
         case inProgress(Task<UIImage, Error>)
+        /// Indicates that the image has been successfully fetched and provides the fetched UIImage.
         case fetched(UIImage)
     }
     
