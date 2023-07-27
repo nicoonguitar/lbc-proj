953fd8351e643378201fe21c5ffbe9a4f5262346
nicoonguitar <nigarcia88@gmail.com>
2023-07-27 20:43:57 +0200

    new: currency and date formatters

--- Lbc/Feature/Listing/ViewModel/ClassifiedAdDetailUIModel.swift
+++ Lbc/Feature/Listing/ViewModel/ClassifiedAdDetailUIModel.swift
@@ -7,16 +7,31 @@ struct ClassifiedAdDetailUIModel: Equatable {
     let description: String
     let image: URL?
     let isUrgent: Bool
-    let price: Float
+    let price: String
     let title: String
 }
 
 extension ClassifiedAdDetailUIModel {
+        
+    static private let currencyFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        // Assumption: the prices are provided in Euro as currency
+        formatter.locale = .init(identifier: "fr_FR")
+        return formatter
+    }()
+    
+    static private let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .short
+        return formatter
+    }()
+    
     static func build(from model: Item, category: Domain.Category?) -> Self {
         .init(
             category: category?.name ?? "",
-            // TODO: map date
-            creationDate: "model.creationDate",
+            creationDate: dateFormatter.string(from: model.creationDate),
             description: model.description,
             image: {
                 if let urlString = model.imagesURL.thumb {
@@ -25,7 +40,7 @@ extension ClassifiedAdDetailUIModel {
                 return nil
             }(),
             isUrgent: model.isUrgent,
-            price: model.price,
+            price: currencyFormatter.string(from: model.price as NSNumber) ?? "",
             title: model.title
         )
     }
