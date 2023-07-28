c3bd743ae3b79c4f54699071f1addf25d986b70f
nicoonguitar <nigarcia88@gmail.com>
2023-07-28 12:04:36 +0200

    new: add note about NSCache

--- Lbc/ImageLoader.swift
+++ Lbc/ImageLoader.swift
@@ -20,6 +20,8 @@ actor ImageLoader {
     
     private let urlSession: URLSession
     
+    // Note: on a larger project, it may be appropiate to use instead a NSCache instance, so the system
+    // will be able to unload the cached data in case its size grows to large in memory.
     private var images: [URL: State]
     
     static let shared: ImageLoader = .init(
